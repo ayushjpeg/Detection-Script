bf7b391c1495b28cb5e9960ccafc9d1c722fcc64
Ayush <ayushjpeg@gmail.com>
2025-02-11 23:14:54 +0530

    Check

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ async def read_status():
     """
     Simulates Broken Object Level Authorization (BOLA)
     """
-    # Randomly deny access (simulating object access issue for unauthorized users)
     if random.choice([True, False]):
         raise HTTPException(
             status_code=403, detail="Access Denied - Broken Object Level Authorization"

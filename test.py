9c00ee3c70dadee6d71aa4669d7e2c5c58060a85
Ayush <ayushjpeg@gmail.com>
2025-02-11 23:43:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 @app.get("/user-service")
 async def read_status():
-    """
-    Simulates Broken Object Level Authorization (BOLA)
-    """
-    if random.choice([True, False]):
-        raise HTTPException(
-            status_code=403, detail="Access Denied - Broken Object Level Authorization"
-        )
-    return {"message": "User Service Accessed Successfully"}
+    return None
